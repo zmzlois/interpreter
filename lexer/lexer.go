7b91d133096389602d5a6975e3bacd383d2fc776
zmzlois <lois.z@comcord.vision>
2024-07-31 01:32:11 +0100

    add more tokens and handle keyword + two chars

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -4,8 +4,8 @@ import "github.com/zmzlois/interpreter/token"
 
 type Lexer struct {
 	input        string
-	position     int  //current position in input (points to current char)
-	readPosition int  //current reading position in input (after current char)
+	position     int  // current position in input (points to current char)
+	readPosition int  // current reading position in input (after current char)
 	ch           byte // current char under examination
 }
 
@@ -20,24 +20,54 @@ func (l *Lexer) readChar() {
 		l.ch = 0
 	} else {
 		l.ch = l.input[l.readPosition]
-
 	}
 	l.position = l.readPosition
 
 	l.readPosition += 1
 }
 
-func (l *Lexer) NextToken() token.Token {
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
+}
 
+func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
 	l.skipWhitespace()
 
 	switch l.ch {
 	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			tok = token.Token{Type: token.EQ, Literal: string(ch) + string(l.ch)}
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
 	case '+':
 		tok = newToken(token.PLUS, l.ch)
+	case '-':
+		tok = newToken(token.MINUS, l.ch)
+	case '!':
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			tok = token.Token{Type: token.NOT_EQ, Literal: string(ch) + string(l.ch)}
+		} else {
+			tok = newToken(token.BANG, l.ch)
+		}
+	case '/':
+		tok = newToken(token.SLASH, l.ch)
+	case '*':
+		tok = newToken(token.ASTERISK, l.ch)
+	case '<':
+		tok = newToken(token.LT, l.ch)
+	case '>':
+		tok = newToken(token.GT, l.ch)
 	case ';':
 		tok = newToken(token.SEMICOLON, l.ch)
 	case '(':
--- token/token.go
+++ token/token.go
@@ -13,13 +13,21 @@ const (
 
 	// Identifiers + literals
 	IDENT = "IDENT" // add, foobar, x, y
-	INT   = "INT"   //1234
+	INT   = "INT"   // 1234
 
 	// Operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+	LT       = "<"
+	GT       = ">"
+	EQ       = "=="
+	NOT_EQ   = "!="
 
-	//Delimiters
+	// Delimiters
 	COMMA     = ","
 	SEMICOLON = ";"
 
@@ -31,11 +39,21 @@ const (
 	// keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
